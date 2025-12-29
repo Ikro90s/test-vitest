18a22977c2039597cb30dcc5e77c23d93988be7b
Icaro Pereira De Paula <136732875+Ikro90s@users.noreply.github.com>
2025-12-29 19:34:46 -0400

    Update container.ts

--- src/config/container.ts
+++ src/config/container.ts
@@ -8,11 +8,9 @@ import { ReportService } from "../domain/services/ReportService";
 
 const container = new Container();
 
-// Singleton para infraestrutura (reuso de instâncias)
 container.bind<ILogger>(TYPES.Logger).to(WinstonLogger).inSingletonScope();
 container.bind<IMailer>(TYPES.Mailer).to(NodemailerMailer).inSingletonScope();
 
-// Transient (padrão) para o serviço de domínio
 container.bind<IReportService>(TYPES.ReportService).to(ReportService);
 
 export { container };
